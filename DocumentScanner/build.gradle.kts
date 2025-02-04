79b98e695acf107374727971c62a4083c9cbef47
Hazzatur <hazzatur@outlook.com>
2025-02-03 19:29:03 -0600

    Update Tiny-OpenCV
    Update dependencies
    Update Tiny-OpenCV (4.11.0) to new repo

--- DocumentScanner/build.gradle.kts
+++ DocumentScanner/build.gradle.kts
@@ -65,7 +65,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.hazzatur"
                 artifactId = "Document-Scanning-Android-SDK"
-                version = "1.2.0"
+                version = "1.2.2"
             }
         }
     }
